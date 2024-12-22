0881a292b05d236734ab61668dbb72613ea0b563
shahramsamar <shahramsamar2010@gmail.com>
2024-12-22 12:29:26 +0330

    update views and use class for register

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 
@@ -11,7 +15,18 @@ class CustomLoginView(LoginView):
         return reverse_lazy('blog:post_list')
 
 
-def RegisterView(request):
-    return render(request,"accounts/register.html")
+class RegisterView(SuccessMessageMixin, FormView):
+    template_name = "registration/register.html"  # Update with the path to your HTML file
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')  # Redirect to login page after successful registration
+    success_message = "Account created successfully! You can now log in."
 
+    def form_valid(self, form):
+        # Save the user instance
+        user = form.save()
+        # Additional processing can be done here
+        return super().form_valid(form)
 
+    def form_invalid(self, form):
+        # Handle errors
+        return super().form_invalid(form)
